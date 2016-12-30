d693f37841195e11c846d58bcc0ba556136ebc77
pcummuskey <pcummuskey@bravurasolutions.com>
2016-12-30 20:50:36 +1300

    Making good progress on getting the build script working.

--- build.rs
+++ build.rs
@@ -3,35 +3,99 @@
 
 #![cfg_attr(feature = "nightly", feature(rustc_private))]
 
-extern crate aster;
-
 #[cfg(feature = "nightly")]
 extern crate syntax;
 
 #[cfg(not(feature = "nightly"))]
 extern crate syntex_syntax as syntax;
 
-use std::env;
+extern crate aster;
+extern crate xml;
+
+use std::env::{ var as env_var, VarError };
 use std::fs::File;
-use std::io::Write;
+use std::io::{ Read, Write, Error as IoError };
 use std::path::Path;
+use std::error::Error;
+use std::fmt::{ Display, Formatter, Result as FmtResult };
 
 use aster::AstBuilder;
 use syntax::print::pprust::expr_to_string;
+use xml::{ Parser, Event };
 
 fn main() {
 
     let builder = AstBuilder::new();
+    let mut parser = Parser::new();
 
-    // read the source schema into expressions.
+    File::open("./schema.rdf").map_err(BuildError::Io)
+        .and_then(file_into_string)
+        .map(|content| parser.feed_str(&content))
+        .unwrap();
+
+    for event in parser {
+        match event.unwrap() {
+            Event::PI(info) => println!("PI:{}", info),
+            Event::ElementStart(tag) => println!("<{}>", tag.name),
+            Event::ElementEnd(tag) => println!("</{}>", tag.name),
+            Event::Characters(chars) => println!("CHARS:{}", chars),
+            Event::CDATA(cdata) => println!("<!CDATA>{}</!CDATA>", cdata),
+            Event::Comment(comment) => println!("<!-- {} -->", comment)
+        }
+    }
 
     let expr = builder.expr()
         .add().u32(1).u32(2);
 
-    let dest = env::var("OUT_DIR").unwrap();
-    let path = Path::new(&dest).join("schema.rs");
-    let mut file = File::create(&path).unwrap();
-
-    file.write_all(expr_to_string(&expr).as_bytes());
+    env_var("OUT_DIR").map_err(BuildError::Var)
+        .map(|dir| Path::new(&dir).join("/schema.rs"))
+        .and_then(|path| file_from_string(&path, expr_to_string(&expr)))
+        .unwrap();
 
 }
+
+fn file_into_string(mut file: File) -> Result<String, BuildError> {
+    let mut output = String::new();
+    return file
+        .read_to_string(&mut output).map_err(BuildError::Io)
+        .map(|len| output);
+}
+
+fn file_from_string<P: AsRef<Path>>(path: P, string: String) -> Result<(), BuildError> {
+    return File::create(&path)
+        .and_then(|mut file| file.write_all(string.as_bytes())).map_err(BuildError::Io);
+}
+
+#[derive(Debug)]
+enum BuildError {
+    Io(IoError),
+    Var(VarError)
+}
+
+impl Display for BuildError {
+    fn fmt(&self, fmtr: &mut Formatter) -> FmtResult {
+        match *self {
+            BuildError::Io(ref err)  => write!(fmtr, "IO error: {}", err),
+            BuildError::Var(ref err) => write!(fmtr, "Var error: {}", err)
+        }
+    }
+}
+
+impl Error for BuildError {
+    
+    fn description(&self) -> &str {
+        match *self {
+            BuildError::Io(ref err)  => err.description(),
+            BuildError::Var(ref err) => err.description()
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            BuildError::Io(ref err)  => Some(err),
+            BuildError::Var(ref err) => Some(err)
+        }
+    }
+
+}
+
